f9f57bebcdb36b8bfe3f44c15a081257797c753b
Bibek Budhathoki <82863609+Bibekbudhathoki@users.noreply.github.com>
2022-07-05 21:05:02 +0545

    Update analysis.R

--- R/analysis.R
+++ R/analysis.R
@@ -3,6 +3,14 @@
 # This is an example function named 'Analysis'
 # which prints 'For biological research'.
 #
+#' @param design Design is experimental technique you used in the experiment. rcbd -->  for randomized complete block  design , rcbd2 --> for 2 factorial  rcbd , rcbd3 --> for 3 factorial rcbd, crd -- > for complete randmized design crd2 --> for 2 factorial crd, crd3 --> for 3 factorial crd, lsd --> for latin square design ,split -- > for split plot design, strip --> for strip plot design  
+#' @param model 0 for no test 1 for LSD 2 for Duncan 3 for HSD test
+#' @param data data frame name
+#' @param rep  replication factor. Block for split plot, if block is not avalable put 0
+#' @param factA Put Treatment name incase of LSD , RCBD and CRD
+#' @param factB Put Second factor Name otherwise put 0 if its absent
+#' @param factC Put third factor Name otherwise put 0 if its absent
+#' @return
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
@@ -26,6 +34,9 @@ analysis <- function(design,model,data,rep, factA,factB,factC){
     q=factB
     s=factC
     m= model
+    print('Congratulation !!! You did it.')
+    print('This package is created by BIBEK BUDHATHOKI with the intend to help biological researchers(specially who conducts field experiment/lab experiment/trials). This package aims to help in analysis in easiest way possible till date. ')
+    print('For any issue related to this package or for neg/positive feedback feel free to mail me BibeloBibek.budhathoki3@gmail.com') 
     for (x in abcd){
       print('####################### This is analysis of of the variable ---> ')
       print(x)
